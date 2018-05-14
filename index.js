2ac593a7c8ae6a00d6c6a8519812f3249a4c53cb
Andy <andersonlavor@gmail.com>
2018-05-14 02:26:21 -0400

    set files to saved to subfolder data

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const url = "ftp://pubftp.spp.org/Markets/DA/BINDING_CONSTRAINTS/2018/05/By_Day/
 const { spawn } = require("child_process");
 const folders = [];
 const ls = spawn('curl', [url]);
-
+const data_folder = './data'; // the folder path to where you want to downalod the files to.
 ls.stdout.on('data', (data) => {
 	folders.push(...data.toString().split('\n'));
 });
@@ -11,14 +11,12 @@ ls.stdout.on('data', (data) => {
 
 ls.on('close', data => {
 
-//	console.log('done', folders);
 	for (let i = 0; i < folders.length; i++) {
 		let temp = folders[i].split(' ');
-//		console.log(temp);
 		if (temp[temp.length - 1].split('.')[1] == 'csv') {
 			const fUrl = `${url}${temp[temp.length - 1]}`;
 			console.log(fUrl);
-			const download = spawn('curl', [fUrl, '--output', '/home/andy/Desktop/Code/snippet/random/js/ftpfiles/data' + temp[temp.length - 1]]);
+			const download = spawn('curl', [fUrl, '--output', `${data_folder}/${temp[temp.length - 1]}`]);
 			download.on('close', data => console.log("File Download -> ", temp[temp.length -1]));
 		}
 	}
